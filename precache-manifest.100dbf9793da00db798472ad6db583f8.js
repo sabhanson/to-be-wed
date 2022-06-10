3569bd7df50f2983c2f646a2cfe166a314d3b46c
Sabrina Hanson <sabhanson7@gmail.com>
2022-06-09 21:33:11 -0700

    Updates

---
+++ precache-manifest.100dbf9793da00db798472ad6db583f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3009acfd57bc22a435d",
+    "url": "/2-be-wed/static/css/main.29d05786.chunk.css"
+  },
+  {
+    "revision": "e3009acfd57bc22a435d",
+    "url": "/2-be-wed/static/js/main.e3009acf.chunk.js"
+  },
+  {
+    "revision": "6d901b3e33806cc9ca52",
+    "url": "/2-be-wed/static/js/1.6d901b3e.chunk.js"
+  },
+  {
+    "revision": "2479d52bec8e0a728699",
+    "url": "/2-be-wed/static/css/2.c378d4d1.chunk.css"
+  },
+  {
+    "revision": "2479d52bec8e0a728699",
+    "url": "/2-be-wed/static/js/2.2479d52b.chunk.js"
+  },
+  {
+    "revision": "35aca0dbca1a983754c4",
+    "url": "/2-be-wed/static/js/runtime~main.35aca0db.js"
+  },
+  {
+    "revision": "2599e0b51d9eace1e5f74c9954ef7ef6",
+    "url": "/2-be-wed/static/media/Kooka-Regular.2599e0b5.ttf"
+  },
+  {
+    "revision": "cfc20b0c17b6742c2819f1e44eac4a98",
+    "url": "/2-be-wed/index.html"
+  }
+];
