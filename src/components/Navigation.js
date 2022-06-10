4dbd55918e83ec42b3dc1f641da14857b786457a
Sabrina Hanson <sabhanson7@gmail.com>
2022-06-10 16:18:52 -0700

    navbar changed to collapsible

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,42 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Navigation.css";
 import { Container, Navbar, Nav } from "react-bootstrap";
 
 function Navigation({ currentPage, handlePageChange }) {
   return (
-    <Container>
-      <Navbar bg="black" expand="md">
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" variant="light" />{" "}
-          Click here for more info
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home" onClick={() => handlePageChange("Home")}>
-                HOME
-              </Nav.Link>
-              <Nav.Link
-                href="#ourStory"
-                onClick={() => handlePageChange("OurStory")}
-              >
-                OUR STORY
-              </Nav.Link>
-              <Nav.Link href="#faq" onClick={() => handlePageChange("FAQ")}>
-                FAQ
-              </Nav.Link>
-              <Nav.Link
-                href="#travel"
-                onClick={() => handlePageChange("Travel")}
-              >
-                TRAVEL
-              </Nav.Link>
-              <Nav.Link href="#rsvp" onClick={() => handlePageChange("RSVP")}>
-                RSVP
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </Container>
+    <Navbar className="justify-content-center" bg="black" expand="lg">
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" variant="light" />{" "}
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" onClick={() => handlePageChange("Home")}>
+              HOME
+            </Nav.Link>
+            <Nav.Link
+              href="#ourStory"
+              onClick={() => handlePageChange("OurStory")}
+            >
+              OUR STORY
+            </Nav.Link>
+            <Nav.Link href="#faq" onClick={() => handlePageChange("FAQ")}>
+              FAQ
+            </Nav.Link>
+            <Nav.Link href="#travel" onClick={() => handlePageChange("Travel")}>
+              TRAVEL
+            </Nav.Link>
+            <Nav.Link href="#rsvp" onClick={() => handlePageChange("RSVP")}>
+              RSVP
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
