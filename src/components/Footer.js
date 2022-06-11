779fba08459bac4fa62fc9eb207364c1fb1c10ca
Sabrina Hanson <sabhanson7@gmail.com>
2022-06-10 17:46:04 -0700

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,55 +1,17 @@
-import React, { useState } from "react";
-import Navigation from "./Navigation";
-import Home from "../pages/Home";
-import OurStory from "../pages/OurStory";
-import FAQ from "../pages/FAQ";
-import Travel from "../pages/Travel";
-import RSVP from "../pages/RSVP";
-import "../styles/Header.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import headerImg from "../images/header.png";
+import React from "react";
+import { Navbar } from "react-bootstrap";
+import "../styles/Footer.css";
+import footerImg from "../images/footer.png";
 
-function Header() {
-  const [currentPage, setCurrentPage] = useState("Home");
-
-  const renderPage = () => {
-    if (currentPage === "Home") {
-      return <Home />;
-    }
-    if (currentPage === "OurStory") {
-      return <OurStory />;
-    }
-    if (currentPage === "FAQ") {
-      return (
-        <FAQ currentPage={currentPage} handlePageChange={handlePageChange} />
-      );
-    }
-    if (currentPage === "Travel") {
-      return <Travel />;
-    }
-    if (currentPage === "RSVP") {
-      return <RSVP />;
-    }
-  };
-
-  const handlePageChange = (page) => setCurrentPage(page);
-
+function Footer() {
   return (
-    <div>
-      <header>
-        <img
-          src={headerImg}
-          alt="'Jayben + Sabrina are getting married! Sunday, the 13th of August, 2023 in Portland, Oregon.' surrounded by rainbow colored flowers"
-          onClick={() => handlePageChange("Home")}
-        />
-      </header>
-      <Navigation
-        currentPage={currentPage}
-        handlePageChange={handlePageChange}
+    <Navbar className="footer">
+      <img
+        src={footerImg}
+        alt="'website created by Sabrina 'almost' Martin, copyright 2022'"
       />
-      {renderPage()}
-    </div>
+    </Navbar>
   );
 }
 
-export default Header;
+export default Footer;
