c7eb6cee68d4f1a8f8374424119fa2e922446328
Sabrina Hanson <sabhanson7@gmail.com>
2022-10-16 14:07:22 -0700

    more work on the carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,45 +1,49 @@
 import Carousel from "react-bootstrap/Carousel";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import React from "react";
 
-import weMet from "../images/we-met.jpg";
+import fishCreekFalls from "../images/STMB.png";
+import SF from "../images/SF.png";
+import nagsHead from "../images/NagsHead.png";
+import winterPark from "../images/WinterPark.png";
 
 function CarouselFade() {
   return (
-    <Carousel fade>
-      <Carousel.Item>
-        <img className="d-block w-50" src={weMet} alt="First slide" />
-        <Carousel.Caption>
-          <h3>First hike together</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <Container className="h-30">
+      <Row className="justify-content-center">
+        <Carousel controls="false" fade>
+          <Carousel.Item>
+            <img
+              className=" w-50"
+              src={fishCreekFalls}
+              alt="Sabrina, Jayben, and their two dogs standing in front of a waterfall"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className=" w-50"
+              src={SF}
+              alt="Sabrina's hand with engagement ring on with Jayben standing in front of the Golden Gate bridge "
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="w-50"
+              src={nagsHead}
+              alt="Sabrina's hand with engagement ring on with Jayben standing in front of the Golden Gate bridge "
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="w-50"
+              src={winterPark}
+              alt="Sabrina's hand with engagement ring on with Jayben standing in front of the Golden Gate bridge "
+            />
+          </Carousel.Item>
+        </Carousel>
+      </Row>
+    </Container>
   );
 }
 
