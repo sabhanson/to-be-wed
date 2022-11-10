9d87e7fdea3d5b0f611ef3c9aef2c5ad4e1d28f3
Sabrina Hanson <sabhanson7@gmail.com>
2022-11-10 11:32:39 -0800

    Updates

---
+++ precache-manifest.820550ec600499ac0e9da368069e166b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb3937722948de1e561e",
+    "url": "/to-be-wed/static/css/main.4d1caf58.chunk.css"
+  },
+  {
+    "revision": "fb3937722948de1e561e",
+    "url": "/to-be-wed/static/js/main.fb393772.chunk.js"
+  },
+  {
+    "revision": "e67e65479d4f06baac99",
+    "url": "/to-be-wed/static/js/1.e67e6547.chunk.js"
+  },
+  {
+    "revision": "d965860c15497b1729a1",
+    "url": "/to-be-wed/static/css/2.2970fad8.chunk.css"
+  },
+  {
+    "revision": "d965860c15497b1729a1",
+    "url": "/to-be-wed/static/js/2.d965860c.chunk.js"
+  },
+  {
+    "revision": "29427d899cc6418db093",
+    "url": "/to-be-wed/static/js/runtime~main.29427d89.js"
+  },
+  {
+    "revision": "5503369ed9261fe9e32521399001060a",
+    "url": "/to-be-wed/static/media/STMB.5503369e.png"
+  },
+  {
+    "revision": "18b644b191afe60653cf7b68520a7733",
+    "url": "/to-be-wed/static/media/SF.18b644b1.png"
+  },
+  {
+    "revision": "6cc3caf7bf5e4a477d87b682d6ec9740",
+    "url": "/to-be-wed/static/media/NagsHead.6cc3caf7.png"
+  },
+  {
+    "revision": "a617623a63548792475b0c99774a886f",
+    "url": "/to-be-wed/static/media/WinterPark.a617623a.png"
+  },
+  {
+    "revision": "cc99d87bcab8fe612120e9b658be84ad",
+    "url": "/to-be-wed/static/media/footer.cc99d87b.png"
+  },
+  {
+    "revision": "2599e0b51d9eace1e5f74c9954ef7ef6",
+    "url": "/to-be-wed/static/media/Kooka-Regular.2599e0b5.ttf"
+  },
+  {
+    "revision": "51fdbaefedb82c5ee25ca8e4d0577adf",
+    "url": "/to-be-wed/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/to-be-wed/precache-manifest.03c826fde6eccbf58d49d393e05295e8.js"
+  "/to-be-wed/precache-manifest.820550ec600499ac0e9da368069e166b.js"
 );
 
 workbox.clientsClaim();
