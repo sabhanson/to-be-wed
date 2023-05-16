31c534a21d835f0a5e8884a6959114d212773fb5
Sabrina Hanson <sabhanson7@gmail.com>
2023-05-15 21:31:15 -0700

    update accommodations

--- src/pages/Accommodations.js
+++ src/pages/Accommodations.js
@@ -5,8 +5,9 @@ const styles = {
   h1: {
     color: "var(--red)",
   },
-  p: {
+  ul: {
     fontSize: "18px",
+    color: 'white'
   },
 };
 
@@ -14,9 +15,16 @@ function Accommodations() {
   return (
     <Container>
       <h1 style={styles.h1}>Accommodations</h1>
-      <p style={styles.p} className="text-white">
-        Check back later :~)
-      </p>
+      <ul
+            style={styles.ul}
+            className=" d-flex justify-content-start list-group list-unstyled m-0"
+          >
+            <li style={styles.ul}><strong>We recommend any of the following hotels:</strong></li>
+            <li style={styles.ul}>- The Heathman Lodge</li>
+            <li style={styles.ul}>- The Hilton - Vancouver</li> 
+            <li style={styles.ul}>- Residence Inn by Marriott - Vancouver</li> 
+            <li style={styles.ul}>- Any Airbnb/VRBO near Vancouver Heights neighborhood</li> 
+          </ul>
     </Container>
   );
 }
